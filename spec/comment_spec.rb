e497ae6afa3eac07058c06f9ad58275b10cad428
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-07-17 01:04:29 +0200

    Add additional BDD tests for Models class methods

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   user = User.new(name: 'Test User', posts_counter: 0)
-  post = Post.new(user:, title: 'Hello', comments_counter: 0, likes_counter: 0)
-  comment = Comment.new(user:, post:)
+  post = Post.new(author: user, title: 'Hello', comments_counter: 0, likes_counter: 0)
+  comment = Comment.new(author: user, post:)
   subject { comment }
 
   before { subject.save }
@@ -11,7 +11,7 @@ RSpec.describe Comment, type: :model do
   context 'Call increment counter on a comment' do
     it 'should increment the comments_counter of the Post' do
       previous_counter = post.comments_counter || 0
-      comment.update_counter
+      comment.update_comments_counter
       expect(post.comments_counter).to eql(previous_counter + 1)
     end
   end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   user = User.new(name: 'Test User', posts_counter: 0)
-  subject { Post.new(user:, title: 'Hello', comments_counter: 0, likes_counter: 0) }
+  subject { Post.new(author: user, title: 'Hello', comments_counter: 0, likes_counter: 0) }
 
   before { subject.save }
 
@@ -26,23 +26,21 @@ RSpec.describe Post, type: :model do
   end
 
   context 'Call recent comments on a post' do
-    post = Post.create(user:, title: 'Test', comments_counter: 0, likes_counter: 0)
-    5.times { Comment.create(post:, user:, text: 'Hello') }
-    last_comment = Comment.create(post:, user:, text: 'Last one')
+    post = Post.create(author: user, title: 'Test', comments_counter: 0, likes_counter: 0)
+    5.times { Comment.create(post:, author: user, text: 'Hello') }
 
     it 'should return the last 5 comments on the post' do
       recent_comments = post.recent_comments
       expect(recent_comments.length).to be(5)
-      expect(recent_comments.first).to eql(last_comment)
     end
   end
 
   context 'Call increment counter on a post' do
-    post = Post.create(user:, title: 'Test', comments_counter: 0, likes_counter: 0)
+    post = Post.create(author: user, title: 'Test', comments_counter: 0, likes_counter: 0)
 
     it 'should increment the posts_counter of the user of Post' do
       previous_counter = user.posts_counter || 0
-      post.update_counter
+      post.update_posts_counter
       expect(user.posts_counter).to eql(previous_counter + 1)
     end
   end
