afc230d50868bf08fa8909b1f10b9c4759d45eee
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-07-23 19:57:02 +0200

    Update controllers to refactor methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,47 @@
 class PostsController < ApplicationController
-  def show
-    puts params
-  end
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
-    puts params
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    render action: '_empty_list' if @posts.empty?
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def get_user_name(author_id)
+    User.find(author_id).name
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_post_url(current_user, @post), notice: 'Post was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
+
+  helper_method :get_user_name
 end
